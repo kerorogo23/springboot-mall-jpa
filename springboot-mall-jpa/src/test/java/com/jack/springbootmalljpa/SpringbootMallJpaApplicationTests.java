2061011e0010def7a1d6d9c2f7e2832430d9b8ed
kerorogo23 <saddjk666@gmail.com>
2023-06-19 22:04:55 +0800

    初始化

---
+++ springboot-mall-jpa/src/test/java/com/jack/springbootmalljpa/SpringbootMallJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jack.springbootmalljpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
